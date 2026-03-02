2c5b5fcdab63d99774224f71692f3eac85f43fd4
Orphan-Crippler <6201093+Orphan-Crippler@users.noreply.github.com>
2026-03-01 23:27:28 -0500

    fix chsh

--- setup.sh
+++ setup.sh
@@ -92,7 +92,7 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     # Manually change the default shell to Zsh for your user
     echo "========================================="
     echo "Changing default shell to Zsh..."
-    sudo chsh -s "$(which zsh)"
+    sudo chsh -s "$(which zsh)" "$USER"
 else
     echo "========================================="
     echo "Oh My Zsh is already installed. Skipping."
